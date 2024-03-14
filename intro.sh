e51e83bbce699f577e7e4f9ea1e6ba7d120ea2a8
Flores0722 <145518304+Flores0722@users.noreply.github.com>
2024-03-14 04:01:02 +0000

    creacion de script intro.sh

---
+++ intro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nombre="Douglas Flores"
+edad=17
+echo su nombre es $nombre y su edad es $edad
+# -ge = Greater or equal
+# -le = Lower or equal
+if [ $edad -ge 18 ]; then 
+echo "usted tiene acceso vip"
+else 
+echo "usted es menor de edad larguese"
+fi
