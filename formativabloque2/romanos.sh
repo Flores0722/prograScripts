db9df72d381ccfa6a6d1dc551544dd4d6ea4e604
Flores0722 <145518304+Flores0722@users.noreply.github.com>
2024-03-24 05:49:29 +0000

    tarea completa

--- formativabloque2/romanos.sh
+++ formativabloque2/romanos.sh
@@ -16,29 +16,62 @@ convertir(){
 # -ne => not equals
 if [ $1 -gt 3999 ]; then
     echo "El número debe ser menor a 4000"
+    echo "Presione cualquier tecla para continuar"
+    read -n 1
     return
 fi
 
-                                # 2148
+                                # 2148      MM C XL VIII
 u=$(( $1 % 10 ))                # extraer las unidades  -> 8
 d=$(( ($1 / 10) % 10 ))         # extraer las decenas   -> 4
 c=$(( ($1 / 100) % 10 ))        # extraer las centenas  -> 1
 m=$(( $1 / 1000 ))              # extraer los millares  -> 2
 
 # Construir el número romano
+resultado=""
     if [ $m -ne 0 ]; then
         # m = 2
-        echo -n ${millares[$m-1]}   # m = 2 -> (2-1)
+        mil=$(echo -n ${millares[$m-1]})
     fi
     if [ $c -ne 0 ]; then
-        # c = 
-        echo -n ${centenas[$c-1]}
+        # c = 1
+        cien=$(echo -n ${centenas[$c-1]})
     fi
     if [ $d -ne 0 ]; then
-        echo -n ${decenas[$d-1]}
+        diez=$(echo -n ${decenas[$d-1]})
     fi
     if [ $u -ne 0 ]; then
-        echo -n ${unidades[$u-1]}
+        # u = 8
+        uni=$(echo -n ${unidades[$u-1]})
     fi
-    echo
+    resultado=$mil$cien$diez$uni
+    echo "El numero $1 en romano se escribe así $resultado"
+    echo "Presione cualquier tecla para regresar al menú"
+    read -n 1
 }
+
+reintentos=0
+
+ingreso(){
+    while true; do
+        read -p "Ingresa un número entero: " numero
+        if ! [[ $numero =~ ^[0-9]+$ ]]; then
+            ((reintentos++))
+            echo -e "\nError: Debes ingresar un número entero."
+            echo -e "Presiona una tecla para continuar...\n"
+            read -n 1 -t 1
+            if [ $reintentos -lt 3 ]; then
+                ingreso
+            else
+                echo "Lo siento, solo es posible convertir desde un número entero!"
+                read -t 2
+                exit 1
+            fi
+        else 
+            convertir $numero
+            break
+        fi
+    done
+}
+
+ingreso
